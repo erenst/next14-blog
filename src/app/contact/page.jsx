a70c49e71a8c3cb571e41f0c022a7acbaaf415b8
erenst <erenst2002@hotmail.com>
2024-02-01 20:36:02 +1030

    add contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./contact.module.css";
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="/contact.png"
+          alt="contact image"
+          fill
+          className={styles.image}
+        />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name and Surname" />
+          <input type="text" placeholder="Email Address" />
+          <input type="text" placeholder="Phone Number (Optional)" />
+          <textarea
+            cols="30"
+            rows="10"
+            name=""
+            id=""
+            placeholder="Message"
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
