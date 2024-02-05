05fc1590dbc743843426e5345f205779311d2d61
erenst <erenst2002@hotmail.com>
2024-02-05 12:12:12 +1030

    use api to fetch data

--- src/app/blogs/[slug]/page.jsx
+++ src/app/blogs/[slug]/page.jsx
@@ -15,12 +15,24 @@ export const generateMetadata = async ({ params }) => {
     description: post.desc,
   };
 };
+// FETCH DATA WITH AN API
+const getData = async (slug) => {
+  const res = await fetch(`http://localhost:3000/api/blogs/${slug}`);
 
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  return res.json();
+};
 const SinglePostPage = async ({ params }) => {
   const { slug } = params;
 
+  // FETCH DATA WITH AN API
+  const post = await getData(slug);
+
   // FETCH DATA WITHOUT AN API
-  const post = await getPost(slug);
+  // const post = await getPost(slug);
 
   return (
     <div className={styles.container}>
--- src/app/blogs/page.jsx
+++ src/app/blogs/page.jsx
@@ -7,8 +7,25 @@ export const metadata = {
   description: "Blog description",
 };
 
+// FETCH DATA WITH AN API
+const getData = async () => {
+  const res = await fetch("http://localhost:3000/api/blogs", {
+    next: { revalidate: 3600 },
+  });
+
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  return res.json();
+};
+
 const BlogPage = async () => {
-  const posts = await getPosts();
+  // FETCH DATA WITH AN API
+  const posts = await getData();
+
+  // FETCH DATA WITHOUT AN API
+  // const posts = await getPosts();
   return (
     <div className={styles.container}>
       {posts.map((post) => (
